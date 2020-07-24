0fa43fb1e7a7733acf536d107c441e6b3cd917a4
Sam M <sammontalvojr@gmail.com>
2020-07-24 11:37:27 -0500

    Deliverable

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,4 +2,5 @@ class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
   validates :rating, :numericality=> true, :inclusion => {:in => 1..5, :message => "should be between 1 and 5"}
+  validates :guest, uniqueness: {scope: [:episode], message: "is already assigned to this date"}
 end
